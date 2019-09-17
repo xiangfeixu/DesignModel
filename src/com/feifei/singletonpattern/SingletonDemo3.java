8fbf1b77e601b543f880eaa3ad421beaf02bd558
许向飞 <xuxf@asiainfo.com>
2019-09-17 10:42:58 +0800

    单例模式双重检查锁优化

--- src/com/feifei/singletonpattern/SingletonDemo3.java
+++ src/com/feifei/singletonpattern/SingletonDemo3.java
@@ -4,6 +4,9 @@ package com.feifei.singletonpattern;
 /**
  * 懒汉模式升级版
  * 双重检查锁定
+ * 这种方式有种缺陷：当有其他线程在初始化实例的时候，做instace==null判断时有可能不为空
+ * 由于java虚拟机的指令重排，可能会返回一个不完整的实例对象
+ * 解决方式：在定义实例变量的时候加上volatile关键字，声明该引用变量是易变的、不稳定的（它的变化的对其他线程可见）
  * @author xuxiangfei
  * @date 2019/8/22
  */
@@ -14,7 +17,7 @@ public class SingletonDemo3 {
     /**
      * 声明一个实例变量
      */
-    private static SingletonDemo3 instance;
+    private static volatile SingletonDemo3 instance;
 
     /**
      * 私有化构造器（核心）
